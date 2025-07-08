3fcb3930eddc7c4a08de2bd0512b2fffc2b06ec8
ndongwoo <ndongwoo@gmail.com>
2025-07-08 17:59:13 +0900

    design

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -12,20 +12,23 @@ export default function Header() {
               {/* 로고 이미지를 여기에 추가할 수 있습니다. 예: <img src="/logo.svg" /> */}
             </div>
             <div className="h-6 text-2xl font-semibold">
-              Hhakorea Blog
+              사랑나눔의사회
             </div>
           </div>
         </Link>
       </div>
       <div className="flex items-center space-x-4 leading-5 mt-4 sm:mt-0 sm:space-x-6">
         <Link href="/blog/1" className="font-medium text-gray-900 dark:text-gray-100">
-          Blog
+          전체 보기
         </Link>
         <Link href="/category" className="font-medium text-gray-900 dark:text-gray-100">
-          Category
+          주제별 보기
         </Link>
         <Link href="/archive" className="font-medium text-gray-900 dark:text-gray-100">
-          Archive
+          연도별 보기
+        </Link>
+        <Link href="https://docs.google.com/forms/d/e/1FAIpQLSdeeAqubMZacUV1Q2yDoRUtZMU26GM7WcgaJJZlN5fkVsnDHg/viewform?usp=preview" target="_blank" className="font-medium text-gray-900 dark:text-gray-100">
+          후원하기
         </Link>
         {/* [추가] 검색창을 헤더의 오른쪽에 추가합니다. */}
         <div className="w-full sm:w-auto">
