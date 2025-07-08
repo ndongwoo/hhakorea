bfd0858a9907e10dab5b5159ce7567fef7676814
ndongwoo <ndongwoo@gmail.com>
2025-07-08 17:49:07 +0900

    new pages

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,10 +9,12 @@ export default function Footer() {
           {/* 소셜 링크 아이콘을 여기에 추가할 수 있습니다. */}
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{`© ${new Date().getFullYear()}`}</div>
+          <div>{`© 2004-${new Date().getFullYear()}` HHA}</div>
           <div>{` • `}</div>
           {/* <a> 태그를 <Link>로 변경합니다. */}
-          <Link href="/">Hhakorea Blog</Link>
+          <Link href="/">Home</Link>
+          <div>{` • `}</div>
+          <Link href="https://www.acrc.go.kr/">공익위반제보</Link>
         </div>
       </div>
     </footer>
--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,11 @@ export default function HomePage() {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            최신 게시글
-          </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Hhakorea 블로그의 최신 소식을 만나보세요.
+            문의전화 02-969-3110
+          </p>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            후원계좌 하나은행 200-000010-04105
           </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
